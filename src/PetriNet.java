b8b0f5ac6491b28138e84829fd0ca017245cff14
GSStoffel <35803172+GSStoffel@users.noreply.github.com>
2022-04-18 09:07:18 -0300

    Update PetriNet.java

--- src/PetriNet.java
+++ src/PetriNet.java
@@ -8,46 +8,24 @@ public class PetriNet {
     List<ResetArc> resetArcs = new ArrayList<>();
     List<InhibitorArc> inhibitorArcs = new ArrayList<>();
 
-    public PetriNet() {
-    }
-
-    public List<Place> getPlaces() {
-        return places;
-    }
-
-    public void setPlaces(List<Place> places) {
-        this.places = places;
-    }
-
-    public List<Transition> getTransitions() {
-        return transitions;
-    }
-
-    public void setTransitions(List<Transition> transitions) {
-        this.transitions = transitions;
-    }
-
-    public List<NormalArc> getNormalArcs() {
-        return normalArcs;
-    }
-
-    public void setNormalArcs(List<NormalArc> normalArcs) {
-        this.normalArcs = normalArcs;
-    }
-
-    public List<ResetArc> getResetArcs() {
-        return resetArcs;
-    }
-
-    public void setResetArcs(List<ResetArc> resetArcs) {
-        this.resetArcs = resetArcs;
-    }
-
-    public List<InhibitorArc> getInibitorArcs() {
-        return inhibitorArcs;
-    }
-
-    public void setInibitorArcs(List<InhibitorArc> inhibitorArcs) {
-        this.inhibitorArcs = inhibitorArcs;
+    public void run(){
+        List<Transition> runnableTransitions = new ArrayList<>();
+        
+        for(Transition transition : transitions) {
+            if(transition.is_runnable()) {
+                runnableTransitions.add(transition)
+            }
+        }
+        
+        if(!runnableTransitions.isEmpty()){
+            for(Transition runnableTransition : runnableTransitions) {
+                for (Arc arc : runnableTransition.getInArcList()) {
+                    arc.run();
+                }
+                for (Arc arc : runnableTransition.getOutArcList()) {
+                    arc.run();
+                }
+            }
+        }
     }
 }
